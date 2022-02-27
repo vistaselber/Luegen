bc882d3fe2da69ad7cc05b40c978caa56bdddaf7
vistaselber <kitfisto90@googlemail.com>
2022-02-27 09:42:07 +0100

    Initial Commit

---
+++ Luegen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Luegen
+//
+//  Created by Jan Hummel on 27.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Luegen/LuegenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuegenApp.swift
+//  Luegen
+//
+//  Created by Jan Hummel on 27.02.22.
+//
+
+import SwiftUI
+
+@main
+struct LuegenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
